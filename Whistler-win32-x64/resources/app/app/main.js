e9954761263e66b4587b9aedfe78252aae9bc9f9
Thomas Tracy <Thomas_Tracy@student.uml.edu>
2016-12-07 21:52:49 -0500

    windows executables

---
+++ Whistler-win32-x64/resources/app/app/main.js
@@ -0,0 +1,61 @@
+//connect the client to the server
+import io from 'socket.io-client';
+var socket = io("http://powerful-gorge-82249.herokuapp.com/", {
+    reconnectionDelay: 0,
+    reconnectionDelayMax: 0,
+    randomizationFactor: 0,
+    timeout: 1000,
+    reconnection: true
+});
+
+
+//when a new user joins, display a welcome message
+socket.on('newUser', function(user, roomID) {
+    $('div#joinForm').remove();
+    $('div#chatBox').append('<p class="important">New user ' + user + ' has joined room ' + roomID + '</p>');
+
+});
+
+//when a new message is recieved from the server, display it with
+//the username that broacasted it
+socket.on('recieveMessage', function(msg, user) {
+    $('div#chatBox').append('<p class="important">' + user + ': </p><p>' + msg + '</p>');
+    $('div#chatBox').animate({
+        scrollTop: $('div#chatBox').height()
+    }, 1000);
+});
+
+//when a new user joins, update the list of users currently in the room
+socket.on('updateUsers', function(list) {
+    $('div#userList').empty();
+    $('div#userList').append('<p>Users:</p>')
+    $.each(list, function(key, value) {
+        $('div#userList').append('<p class="important">' + value + '</p>');
+    });
+});
+
+socket.on('userDisconnected', function(username) {
+    $('div#chatBox').append('<p class="important">User ' + username + ' has disconnected </p>');
+});
+
+//submit the form to join a room
+$('button#join').click(function() {
+    $('div#joinModal').hide();
+    var username = $('input#username').val();
+    var roomID = $('input#roomID').val();
+    socket.emit('joinRoom', username, roomID);
+
+})
+
+//submit a message
+$("input#textBar").keyup(function(event){
+        if(event.keyCode == 13){
+            $("button#sendMessage").click();
+        }
+});
+$('button#sendMessage').click(function() {
+    var msg = $('input#textBar').val();
+    //clear the text box
+    $('input#textBar').val('');
+    socket.emit('sendMessage', msg);
+});
